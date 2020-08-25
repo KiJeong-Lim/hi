23fad36b6cd2e4584932526cc432bd62764516f6
KiJeong-Lim <gijungdduk@naver.com>
2020-08-25 20:55:49 +0900

    .

--- src/Hi/Header.hs
+++ src/Hi/Header.hs
@@ -10,6 +10,17 @@ type CoreBind var annot = (var, CoreTerm var annot)
 
 type CoreAlts var annot = [((Constructor, [CoreBind varr annot]), CoreTerm var annot)]
 
+data TyUnivIdx
+    = TUIZero
+    | TUISucc TyUnivIdx
+    | TUIMVar MetaVar
+    deriving (Eq)
+
+data Sort
+    = SType TyUnivIdx
+    | SProp
+    deriving (Eq)
+
 data CoreTerm var annot
     = CVar annot var
     | CCon annot Constructor
@@ -17,5 +28,6 @@ data CoreTerm var annot
     | CLam annot (CoreBind var annot) (CoreTerm var annot)
     | CAll annot (CoreBind var annot) (CoreTerm var annot)
     | CMat annot (CoreTerm var annot) (CoreAlts var annot)
+    | CSrt annot Sort
     | MVar annot MetaVar
     deriving (Eq)
